9ad815e8b6b188d666113386ac2942c8e5ae2db7
Chloe <chloefdez@gmail.com>
2025-10-02 19:16:14 -0700

    feat(NewItems): API integration, carousel, skeleton loading, countdown, and shared slider styles

--- src/lib/api.js
+++ src/lib/api.js
@@ -8,3 +8,8 @@ export async function fetchHotCollections(signal) {
   const res = await http.get("hotCollections", { signal });
   return res.data; 
 }
+
+export async function fetchNewItems(signal) {
+  const res = await http.get("newItems", { signal });
+  return res.data || [];
+}
